9967fdedec1136e81e1cb0fb89766cd2208f4cc5
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-08 12:22:16 -0800

    Created and initialized the header file for Select_And

---
+++ Select_And.hpp
@@ -0,0 +1,16 @@
+#ifndef SELECT_AND_HPP
+#define SELECT_AND_HPP
+
+#include "select.hpp"
+
+class Select_And{
+
+	private:
+
+	public:
+
+		~Select_And()
+		bool select(const Spreadsheet* sheet, int row);
+};
+
+#endif
