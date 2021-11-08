3f4cd3b65bf9086f374c98b468e6a766e65caa1e
RDanover <danover.rachel@gmail.com>
2021-11-08 12:19:15 -0800

    Initialized Select_Contains.hpp and Select_Contains.cpp

--- Select_Contains.cpp
+++ Select_Contains.cpp
@@ -1,1 +1,12 @@
 #include "Select_Contains.hpp"
+
+Select_Contains::~Select_Contains{
+
+}
+
+bool Select_Contains::select(const Spreadsheet* sheet, int row){
+
+
+	return true;
+}
+
---
+++ Select_Contains.hpp
@@ -0,0 +1,16 @@
+#ifndef SELECT_CONTAINS_HPP
+#define SELECT_CONTAINS_HPP
+
+#include "select.hpp"
+
+class Select_Contains{
+
+	private:
+
+	public:
+
+		~Select_Contains()
+		bool select(const Spreadsheet* sheet, int row);
+};
+
+#endif
