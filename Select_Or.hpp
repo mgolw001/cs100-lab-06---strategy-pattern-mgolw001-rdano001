b61f51371d98923837314cc442b1bc0fea7bdc78
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-08 12:23:32 -0800

    Created and initialized the header for the Select_Or class

---
+++ Select_Or.hpp
@@ -0,0 +1,16 @@
+#ifndef SELECT_OR_HPP
+#define SELECT_OR_HPP
+
+#include "select.hpp"
+
+class Select_Or{
+
+	private:
+
+	public:
+
+		~Select_Or()
+		bool select(const Spreadsheet* sheet, int row);
+};
+
+#endif
