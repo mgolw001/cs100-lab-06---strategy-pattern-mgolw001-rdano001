84a257784da1a11aa94f5d218fce90186c60f990
mgolw001 <mgolw001@ucr.edu>
2021-11-12 10:55:22 -0800

    added includes

--- test.cpp
+++ test.cpp
@@ -1,7 +1,13 @@
 #include "spreadsheet.hpp"
 
 #include "gtest/gtest.h"
-
+#include "spreadsheet.hpp"
+#include "Select_Or_Test.hpp"
+#include "Select_And_Test.hpp"
+#include "Select_Contains_Test.hpp"
+#include "Select_Not_Test.hpp"
+#include "select.hpp"
+#include "Select_Contains.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
